302ebdd59c555ff9391c4e9a379d8de18858f1fe
Alaba Olanrewaju <alabaolanrewaju13@gmail.com>
2021-08-21 15:20:09 +0100

    commit

--- my_resume_project/settings.py
+++ my_resume_project/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 import django_heroku
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'maqlrzpk6tkd0=%eto&m%f@ic(m(i@pg^1g9&@9^3nu#puw8k^'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
